8e6dabcc16252140c937097d9863b5e100e80de7
Mikael <widell79@gmail.com>
2021-03-02 15:10:06 +0100

    feat: Books filter by shelf

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -2,46 +2,58 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Book from "./Book";
 
-const BookShelf = (props) => {
+const BookShelf = ({ books }) => {
   return (
     <div className="bookshelf-books">
       <div className="list-books-content">
         <h2>Currently Reading</h2>
         <ol className="books-grid">
-          {props.books.map((book) => (
-            <Book
-              image={book.imageLinks}
-              bookTitle={book.title}
-              bookAuthor={book.authors}
-              key={book.id}
-            />
-          ))}
+          {books
+            .filter(function(book) {
+              return book.shelf === "currentlyReading";
+            })
+            .map((book) => (
+              <Book
+                image={book.imageLinks}
+                bookTitle={book.title}
+                bookAuthor={book.authors}
+                key={book.id}
+              />
+            ))}
         </ol>
       </div>
       <div className="list-books-content">
         <h2>Want to Read</h2>
         <ol className="books-grid">
-          {props.books.map((book) => (
-            <Book
-              image={book.imageLinks}
-              bookTitle={book.title}
-              bookAuthor={book.authors}
-              key={book.id}
-            />
-          ))}
+          {books
+            .filter(function(book) {
+              return book.shelf === "wantToRead";
+            })
+            .map((book) => (
+              <Book
+                image={book.imageLinks}
+                bookTitle={book.title}
+                bookAuthor={book.authors}
+                key={book.id}
+              />
+            ))}
         </ol>
       </div>
       <div className="list-books-content">
         <h2>Read</h2>
         <ol className="books-grid">
-          {props.books.map((book) => (
-            <Book
-              image={book.imageLinks}
-              bookTitle={book.title}
-              bookAuthor={book.authors}
-              key={book.id}
-            />
-          ))}
+          {books
+            .filter(function(book) {
+              return book.shelf === "read";
+            })
+            .map((book) => (
+              <Book
+                image={book.imageLinks}
+                bookTitle={book.title}
+                bookAuthor={book.authors}
+                key={book.id}
+              />
+            ))}
         </ol>
       </div>
       <div className="open-search">
